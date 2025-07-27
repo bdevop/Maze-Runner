d1e46b6fc801e105a6ac28c067dea85c32d31db2
Jacob Polletta <Jacob@letta.cloud>
2025-07-27 19:24:29 -0400

    lets try this again :)

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,53 @@ const c = document.getElementById("gamecanvas") as HTMLCanvasElement;
 // This is the tool used to paint on the canvas
 const ctx = c.getContext("2d") as CanvasRenderingContext2D;
 
-let x = c.width / 2;
-let y = c.height - 30;
+let player = { 
+    x: 1,
+    y: 350,
+    width: 50, 
+    height: 50,
+    speed: 5,
+    jumping: false,
+};
 
-// interval the ball will move 
-let dx = 2;
-let dy = -2;
+function drawPlayer() {
+    ctx.fillStyle = "blue"
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
 
-function draw() {
-    console.log(x);
-    ctx.beginPath();
-    ctx.rect(x, y, 50, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();
-    x += dx;
-    y += dy;
-} 
+function movePlayer() { 
+    
+}
+    
 
-setInterval(draw, 10);
+function gameLoop() { 
+drawPlayer()
+}
+requestAnimationFrame(gameLoop);
+
+
+
+
+
+
+
+
+// let x = c.width / 2;
+// let y = c.height - 30;
+
+// // interval the ball will move 
+// let dx = 2;
+// let dy = -2;
+
+// function draw() {
+//     ctx.clearRect(0, 0, c.width, c.height);
+//     ctx.beginPath();
+//     ctx.rect(x, y, 50, 50);
+//     ctx.fillStyle = "#FF0000";
+//     ctx.fill();
+//     ctx.closePath();
+//     x += dx;
+//     y += dy;
+// } 
+
+// setInterval(draw, 10);
